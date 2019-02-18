b175b6e34f896ff268d50f066d9eb126810ca0c9
Dylan Garrett <dylan.garrett@gmail.com>
2019-02-17 20:31:22 -0800

    Some initial wifi AP code

--- BirbTweeter_Sender/BirbTweeter_Sender.ino
+++ BirbTweeter_Sender/BirbTweeter_Sender.ino
@@ -1,4 +1,7 @@
 #include "heltec.h"
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiAP.h>
 
 #define BAND    433E6  //you can set band here directly,e.g. 868E6,915E6
 
@@ -6,6 +9,11 @@ unsigned int counter = 0;
 String rssi = "RSSI --";
 String packSize = "--";
 String packet ;
+
+const char *ssid = "BirbTweeterSender";
+//const char *password = "yourPassword";
+
+WiFiServer server(80);
 
 void setup()
 {
@@ -18,10 +26,18 @@ void setup()
   
   Heltec.display->drawString(0, 0, "Heltec.LoRa Initial success!");
   Heltec.display->display();
+
+  WiFi.softAP(ssid);
+  IPAddress myIP = WiFi.softAPIP();
+  Heltec.display->clear();
+  Heltec.display->drawString(0, 0, String("AP IP address: ") + String(myIP));
+  Heltec.display->display();
+  server.begin();
+  
   delay(1000);
 }
 
-void loop()
+void oldloop()
 {
   Heltec.display->clear();
   Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
@@ -42,4 +58,59 @@ void loop()
   delay(1000);                       // wait for a second
   digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
+}
+
+void loop() {
+  WiFiClient client = server.available();   // listen for incoming clients
+
+  if (client) {                             // if you get a client,
+    Serial.println("New Client.");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("Click <a href=\"/H\">here</a> to turn ON the LED.<br>");
+            client.print("Click <a href=\"/L\">here</a> to turn OFF the LED.<br>");
+            client.print("<form action=\"/send\" method=\"post\">");
+            client.print("To Send: <input type=\"text\" name=\"tosend\"><br>");
+            client.print("<input type=\"submit\" value=\"Submit\">");
+            client.print("</form>");
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {    // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /H")) {
+          //digitalWrite(LED_BUILTIN, HIGH);               // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /L")) {
+          //digitalWrite(LED_BUILTIN, LOW);                // GET /L turns the LED off
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("Client Disconnected.");
+  }
 }
