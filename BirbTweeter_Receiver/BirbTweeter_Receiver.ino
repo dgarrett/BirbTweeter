3720165cdd4be7d5499327690e92b67229298505
Dylan Garrett <dylan.garrett@gmail.com>
2019-10-02 21:05:05 -0700

    Add comments

--- BirbTweeter_Receiver/BirbTweeter_Receiver.ino
+++ BirbTweeter_Receiver/BirbTweeter_Receiver.ino
@@ -2,40 +2,56 @@
 #include "Adafruit_Thermal.h"
 
 #define BAND    433E6  //you can set band here directly,e.g. 868E6,915E6
-String rssi = "RSSI --";
+String rssi = "RSSI --"; // Will store signal strength
 String packSize = "--";
-String packet ;
+String packet;
 
 // Set to 0 if you aren't connecting a printer
 #define PRINTER 1
 
+// Use thermal printer on Serial port 2 with DTR pin 13
+// DTR is used for the printer to tell us when it's ready
+// for more data
 Adafruit_Thermal printer(&Serial2, 13);
 
 void HeltecLoRaData(){
+  // Reset the OLED display
   Heltec.display->clear();
   Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
   Heltec.display->setFont(ArialMT_Plain_10);
+
+  // Print size of packet and signal strength on OLED
   Heltec.display->drawString(0 , 0 , "Got "+ packSize + " B, " + rssi);
+  // Print message on OLED
   Heltec.display->drawStringMaxWidth(0 , 11 , 130, packet);
   Heltec.display->display();
 #if PRINTER
+  // Print message on thermal printer
   printer.println(packet);
+  // Feed the thermal paper out a little bit
   printer.feed(1);
 #endif
 }
 
 void cbk(int packetSize) {
+  // Reset our packet variable
   packet ="";
+  // Get the size of our received packet
   packSize = String(packetSize,DEC);
+  // Read every byte received from LoRa
   for (int i = 0; i < packetSize; i++) { packet += (char) Heltec.LoRa.read(); }
-  rssi = "RSSI " + String(Heltec.LoRa.packetRssi(), DEC) ;
+  // Store the signal strength as a string to print
+  rssi = "RSSI " + String(Heltec.LoRa.packetRssi(), DEC);
+
+  // Call our data handling callback
   HeltecLoRaData();
 }
 
 void setup() { 
    //WIFI Kit series V1 not support Vext control
   Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.Heltec.Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
- 
+
+  // Setup OLED display
   Heltec.display->init();
   Heltec.display->flipScreenVertically();  
   Heltec.display->setFont(ArialMT_Plain_10);
@@ -44,22 +60,26 @@ void setup() {
   Heltec.display->drawString(0, 10, "Wait for incoming data...");
   Heltec.display->display();
 
+  // Setup thermal printer to print using Serial port 2
 #if PRINTER
   Serial2.begin(9600); // Printer
   printer.begin();
 
   printer.println(F("Printer init success"));
+  // Feed the printer paper out a little bit
   printer.feed(2);
 
   printer.setDefault();
 #endif
   
   delay(1000);
-  //Heltec.LoRa.onReceive(cbk);
+
+  // Turn on LoRa to receive
   Heltec.LoRa.receive();
 }
 
 void loop() {
+  // Check if we received a LoRa packet, call `cbk()` if we did
   int packetSize = Heltec.LoRa.parsePacket();
   if (packetSize) { cbk(packetSize);  }
   delay(10);
--- BirbTweeter_Sender/BirbTweeter_Sender.ino
+++ BirbTweeter_Sender/BirbTweeter_Sender.ino
@@ -6,13 +6,8 @@
 
 #define BAND    433E6  //you can set band here directly,e.g. 868E6,915E6
 
-unsigned int counter = 0;
-String rssi = "RSSI --";
-String packSize = "--";
-String packet ;
-
+// WiFi hotspot name
 const char *ssid = "BirbTweeterSender";
-//const char *password = "yourPassword";
 
 const char *html_root =
 #include "index.html.h"
@@ -32,32 +27,44 @@ void setup()
   Heltec.display->drawString(0, 0, "Heltec.LoRa Initial success!");
   Heltec.display->display();
 
+  // Setup WiFi hotspot and print IP on OLED screen
   WiFi.softAP(ssid);
   IPAddress myIP = WiFi.softAPIP();
   Heltec.display->clear();
   Heltec.display->drawString(0, 0, String("AP IP address: ") + myIP.toString());
   Heltec.display->display();
 
+  // Send our HTML when the user navigates to our IP address
   server.on("/", []() {
     server.send(200, "text/html", html_root);
   });
+
+  // Our HTML submits the form with a POST to /send
   server.on("/send", HTTP_POST, []() {
     Serial.println("Args: ");
     Serial.println(server.args());
     Serial.println(server.arg("tosend"));
     Serial.println(server.arg("currenttime"));
+
+    // Return the user to our home page
     server.send(200, "text/html", html_root);
+
+    // Display the user's message on our OLED screen
     Heltec.display->drawString(0, 10, String(server.arg("tosend")));
     Heltec.display->display();
 
+    // Create a packet and send the message over LoRa
     Heltec.LoRa.beginPacket();
     Heltec.LoRa.print(server.arg("currenttime") + "\n");
     Heltec.LoRa.print(server.arg("tosend"));
     Heltec.LoRa.endPacket();
   });
+
+  // Start server on WiFi with the above configuration
   server.begin();
 }
 
 void loop(void) {
+  // Let the server library process requests
   server.handleClient();
 }
